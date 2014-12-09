a3e707f69932e4bfe35b8dbe9b82bd02347d345c
Annemarie Ketola <star7anne@yahoo.com>
2014-12-09 13:16:35 -0800

    Initial Commit

---
+++ HigherOrLowerGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HigherOrLowerGame
+//
+//  Created by Annemarie Ketola on 12/9/14.
+//  Copyright (c) 2014 UpEarly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
